bb68c1e3c32c6c9b5780e127e45d1aba14e05e7d
Ryan Schlesinger <ryan@ryanschlesinger.com>
2020-11-25 12:14:29 -0800

    Fix shellcheck warnings

--- lib/cache-keys.bash
+++ lib/cache-keys.bash
@@ -12,9 +12,7 @@ function find_cache() {
 
   keys=("${@}")
 
-  output=$(docker build --pull -t docker-cache-buildkite-plugin:find-cache "${BASEDIR}/ruby" 2>&1)
-
-  if [[ $? != 0 ]]; then
+  if ! output=$(docker build --pull -t docker-cache-buildkite-plugin:find-cache "${BASEDIR}/ruby" 2>&1); then
     >&2 echo "docker build failed:"
     >&2 echo "${output}"
     exit 2
@@ -24,13 +22,13 @@ function find_cache() {
     --log-level "error" \
     run \
     --rm \
-    -e AWS_VAULT=${AWS_VAULT:-} \
-    -e AWS_REGION=${AWS_REGION:-} \
-    -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-} \
-    -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-} \
-    -e AWS_SESSION_TOKEN=${AWS_SESSION_TOKEN:-} \
-    -e AWS_SECURITY_TOKEN=${AWS_SECURITY_TOKEN:-} \
-    -e AWS_SESSION_EXPIRATION=${AWS_SESSION_EXPIRATION:-} \
+    -e AWS_VAULT="${AWS_VAULT:-}" \
+    -e AWS_REGION="${AWS_REGION:-}" \
+    -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID:-}" \
+    -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY:-}" \
+    -e AWS_SESSION_TOKEN="${AWS_SESSION_TOKEN:-}" \
+    -e AWS_SECURITY_TOKEN="${AWS_SECURITY_TOKEN:-}" \
+    -e AWS_SESSION_EXPIRATION="${AWS_SESSION_EXPIRATION:-}" \
     docker-cache-buildkite-plugin:find-cache \
     ruby -I . cli.rb \
     "$bucket" "$prefix" "${keys[@]}"
